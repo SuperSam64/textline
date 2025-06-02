4af287a22fff31a5e2bfa6ec352d1c3509764724
SuperSam64 <165439396+SuperSam64@users.noreply.github.com>
2025-06-02 07:37:51 -0400

    Update add_contact.js

--- add_contact.js
+++ add_contact.js
@@ -1,1 +1,14 @@
 
+var contact=getParams();
+coneole.log(contact);
+function getParams(){
+  var queryString=window.location.search;
+  var urlParams=new URLSearchParams(queryString);
+  return {
+    name:URLSearchParams.getAll('name'),
+    phone:URLSearchParams.getAll('phone'),
+    email:URLSearchParams.getAll('email'),
+    CID:URLSearchParams.getAll('CID'),
+    order:URLSearchParams.getAll('order')
+  }
+}
